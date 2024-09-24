58851d1c8fe548aa510d8a83841796199f260fde
Rjamez <robin.adhola.student@moringaschool.com>
2024-09-24 11:42:23 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+ const employee = {
+    name:"Brian",
+    streetAddress:"Kawangware",
+ }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee ={...employee};
+    newEmployee.name ="Sam";
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeClone = {...employee};
+    delete employeeClone.name;
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
